c1860f9c5b5d06841c4be72fef897ba47a0f60b0
Joshua <joshua@brandforge.studio>
2025-02-28 12:12:33 -0500

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let titles = [];
+  for (const book of books) {
+    titles.push(book.title);
+  };
+  return titles
 };
+
+//  example
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
 
 // Do not edit below this line
 module.exports = getTheTitles;
