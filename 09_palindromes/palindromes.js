ba425e4de6abb566b916d4464df0f90e1de73866
Joshua <joshua@brandforge.studio>
2025-02-28 11:57:18 -0500

    Update palindromes.js
    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes =  (str) => {
+   let removeSpace = str.replaceAll(' ', '');
+   let forwards = str.toCharArray();
+   let backwards = forwards.reverse();
+  if (backwards === forwards) {
+    console.log("It's a palendrome!");
+    return true;
+    } else {
+    console.log("It's not a palindrome!");
+    return false;
+  };
 };
 
 // Do not edit below this line
