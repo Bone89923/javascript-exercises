61005ec53724e2534708b15f785f9b89b66a1f51
Joshua <joshua@brandforge.studio>
2025-02-28 14:27:22 -0500

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+  let peopleAges = []; 
+  people.map((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    };
+    peopleAges.push(person.yearOfDeath - person.yearOfBirth);
+  });
+  let oldestPerson = peopleAges.indexOf(Math.max(...peopleAges));
+  return people[oldestPerson];
 };
 
 // Do not edit below this line
